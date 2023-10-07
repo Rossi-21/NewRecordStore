1ea029a9dbed9d921df6dceabb28aacce7e54069
Ross Palmer <rossi21@gmail.com>
2023-10-07 06:39:53 -0700

    Login Logout

--- record_app/views.py
+++ record_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CreateUserForm
 from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import *
 
 def home(request):
@@ -37,7 +38,23 @@ def registerUser(request):
     return render(request, 'register.html', context)
 
 def loginUser(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
     context={}
+
     return render(request, "login.html", context)
 
-# testing git 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
